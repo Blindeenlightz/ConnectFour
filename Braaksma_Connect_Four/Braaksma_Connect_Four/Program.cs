499a6c55d4b7ae414bd41cc5a539ad7555cec800
Jamie Braaksma <j.braaksma331@mybvc.ca>
2021-04-11 00:28:03 -0600

    * Fixed players tokens beign able to overlap one another.  * Added loop to prompt for new selection if column is full  * Removed unused board declaration in Program  * Added loop to entire game for rematches  * Added Tie detector  * Added winning state algorithm and logic to end game if true  * Need to test all game states to test winning algorithm

--- Braaksma_Connect_Four/Braaksma_Connect_Four/Program.cs
+++ Braaksma_Connect_Four/Braaksma_Connect_Four/Program.cs
@@ -1,5 +1,4 @@
 ﻿/*
-
  */
 
 using System;
@@ -33,6 +32,7 @@ namespace Braaksma_Connect_Four
             _difficulty = difficulty;
         }
     }
+
 
     class Human : Player
     {
@@ -75,7 +75,7 @@ namespace Braaksma_Connect_Four
 
         public void Turn(Player currentPlayer)
         {
-            Console.WriteLine("It's {0}'s turn!", currentPlayer.Name);
+            Console.WriteLine("It's {0}'s turn!\n", currentPlayer.Name);
             turnCount++;
         }
 
@@ -83,12 +83,12 @@ namespace Braaksma_Connect_Four
         {
             int counter = 0;
             foreach (char item in Cells)
-            {       
-                if(counter % Columns == 0 && counter != 0)
+            {
+                if (counter % Columns == 0 && counter != 0)
                     Console.Write("\n");
 
                 Console.Write("{0} ", item);
-                
+
                 counter++;
             }
 
@@ -99,92 +99,154 @@ namespace Braaksma_Connect_Four
                 Console.Write("{0} ", i + 1);
 
             }
-            Console.WriteLine("\n\nSelect a number between 1 - {0} to place your piece", Columns);
         }
 
-        public void InsertToken(Player currentPlayer, int selection) //doesn't change the global currentGameBoard
+        public void InsertToken(Player currentPlayer, int selection)
         {
-            //Replace the blank square with the players token in the gameboard (<List>)
-            for (int i = (Cells.Count - 1) - (Columns - selection); i > 0 ; i -= Columns)
+            bool columnFull = true;
+            do
             {
-                if(Cells[i] != currentPlayer.Token)
+                //Replace the blank square with the players token in the gameboard (<List>)
+                for (int i = (Cells.Count - 1) - (Columns - selection); i > 0; i -= Columns)
                 {
-                    Cells[i] = currentPlayer.Token;
-                    Console.WriteLine(Cells[i]);
-                    break;
+                    if (Cells[i] != currentPlayer.Token && Cells[i] == '#')
+                    {
+                        Cells[i] = currentPlayer.Token;
+                        columnFull = false;
+                        break;
+                    }
+                }
+
+                if (columnFull)
+                {
+                    Console.WriteLine("That column is full please pick another");
+                    int.TryParse(Console.ReadLine(), out selection);
+                }
+            } while (columnFull);
+        }
+
+        public bool Winner(Player player)
+        {
+
+            for(int i = 0; i < Cells.Count; i++)
+            {
+                //Check horizontal
+                //Validate a win is within range
+                if(i + 3 < Cells.Count)
+                {
+                    if (Cells[i] == player.Token && Cells[i + 1] == player.Token && Cells[i + 2] == player.Token && Cells[i + 3] == player.Token)
+                        return true;
+                }
+
+                //Check Vertical
+                //Validate a win is within range
+                if (i + (Columns * 3) < Cells.Count)
+                {
+                    if (Cells[i] == player.Token && Cells[i + Columns] == player.Token && Cells[i + (Columns * 2)] == player.Token && Cells[i + (Columns * 3)] == player.Token)
+                        return true;
+                }
+
+                if (i % 7 == 0)
+                {
+                    for (int k = i; k < 4; k++)
+                    {
+                        //Validate a win is within range
+                        if (i + ((Columns + 1) * 3) < Cells.Count)
+                        {
+                            //Check Diagonal
+                            if (Cells[k] == player.Token && Cells[k + (Columns + 1)] == player.Token && Cells[k + ((Columns + 1) * 2)] == player.Token && Cells[k + ((Columns + 1) * 3)] == player.Token)
+                                return true;
+                        }
+                    }
+                    for (int j = i + 3; j < i + 7; j++)
+                    {
+                        //Validate a win is within range
+                        if (i + ((Columns - 1) * 3) < Cells.Count)
+                        {
+                            //Check reverse Diagonal
+                            if (Cells[j] == player.Token && Cells[j + (Columns - 1)] == player.Token && Cells[j + ((Columns - 1) * 2)] == player.Token && Cells[j + ((Columns - 1) * 3)] == player.Token)
+                                return true;
+                        }
+                    }
                 }
             }
+            return false;
         }
     }
+
 
     class Program
     {
         static void Main(string[] args)
         {
-            List<char> board = new List<char>();
-            //Choose the size of board to play with
-            int numberOfColumns = 7;
-            int numberOfRows = 6;
-            int choice;
-            int numberPlayers;
-            bool winner = false;
-
-            Console.WriteLine("One player game or Two? (1 / 2)");
-            int.TryParse(Console.ReadLine(), out numberPlayers);
-
-            //Validate number of players
-            while (numberPlayers < 1 && numberPlayers > 2)
+            bool again = true;
+            do
             {
-                Console.WriteLine("Please enter the number of players. Either 1 or 2");
+                //Choose the size of board to play with
+                int numberOfColumns = 7;
+                int numberOfRows = 6;
+                int choice;
+                int numberPlayers;
+                string theWinner;
+                bool winner = false;
+
+                Console.WriteLine("One player game or Two? (1 / 2)");
                 int.TryParse(Console.ReadLine(), out numberPlayers);
-                Console.WriteLine(numberPlayers);
-            }
+
+                //Validate number of players
+                while (numberPlayers < 1 && numberPlayers > 2)
+                {
+                    Console.WriteLine("Please enter the number of players. Either 1 or 2");
+                    int.TryParse(Console.ReadLine(), out numberPlayers);
+                    Console.WriteLine(numberPlayers);
+                }
 
 
 
-            //Single Player Game
-            if(numberPlayers == 1)
-            {
+                //Single Player Game
+                if (numberPlayers == 1)
+                {
                     Console.WriteLine("Enter your name");
                     string player1Name = Console.ReadLine();
 
                     Console.WriteLine("What difficulty would you like? ( 1 / 2 / 3");
                     int difficulty = Console.Read();
-                
+
                     //Validate difficulty
-                    while(difficulty != 1 || difficulty != 2 || difficulty != 3)
+                    while (difficulty != 1 || difficulty != 2 || difficulty != 3)
                     {
                         Console.WriteLine("Please enter 1 or 2 or 3");
                         difficulty = Console.Read();
                     }
-                
+
                     Player player1 = new Human(player1Name, 'X');
-                    Player player2= new AI("Computer", difficulty, '0');
+                    Player player2 = new AI("Computer", difficulty, '0');
 
                     Gameplay singlePlayerGame = new Gameplay(numberOfRows, numberOfColumns);
 
-
-                do
-                {
-                    singlePlayerGame.DrawBoard();
-
-                    //Get player selection
-                    int.TryParse(Console.ReadLine(), out choice);
-
-                    //Validate player selection
-                    while (choice < 1 || choice > numberOfColumns)
+                    do
                     {
-                        Console.WriteLine("Please enter a valid number between 1 - {0}", numberOfColumns);
+                        singlePlayerGame.DrawBoard();
+                        Console.WriteLine("\n\nSelect a number between 1 - {0} to place your piece", singlePlayerGame.Columns);
+
+                        //Get player selection
                         int.TryParse(Console.ReadLine(), out choice);
-                    }
+
+                        //Validate player selection
+                        while (choice < 1 || choice > numberOfColumns)
+                        {
+                            Console.WriteLine("Please enter a valid number between 1 - {0}", numberOfColumns);
+                            int.TryParse(Console.ReadLine(), out choice);
+                        }
 
 
-                }while(!winner);
-            }
+                    } while (!winner);
+                }
 
-            //Multiplayer Game
-            else if(numberPlayers == 2)
-            {
+
+                //Multiplayer Game
+                else if (numberPlayers == 2)
+                {
                     Console.WriteLine("Enter your name player 1");
                     string player1Name = Console.ReadLine();
 
@@ -193,35 +255,71 @@ namespace Braaksma_Connect_Four
 
                     Player player1 = new Human(player1Name, 'X');
                     Player player2 = new Human(player2Name, '0');
-            
+
                     Gameplay multiplayerGame = new Gameplay(numberOfRows, numberOfColumns);
-                
-                do
-                {
-                    if(Gameplay.turnCount % 2 == 0)
-                        multiplayerGame.Turn(player1);
-                    else
-                        multiplayerGame.Turn(player2);
 
-                    multiplayerGame.DrawBoard();
-
-                    //Get player selection
-                    int.TryParse(Console.ReadLine(), out choice);
-
-                    //Validate player selection
-                    while (choice < 1 || choice > numberOfColumns)
+                    do
                     {
-                        Console.WriteLine("Please enter a valid number between 1 - {0}", numberOfColumns);
+                        if (Gameplay.turnCount % 2 == 0)
+                            multiplayerGame.Turn(player1);
+                        else
+                            multiplayerGame.Turn(player2);
+
+                        multiplayerGame.DrawBoard();
+                        Console.WriteLine("\n\nSelect a number between 1 - {0} to place your piece", multiplayerGame.Columns);
+
+                        //Get player selection
                         int.TryParse(Console.ReadLine(), out choice);
+
+                        //Validate player selection
+                        while (choice < 1 || choice > numberOfColumns)
+                        {
+                            Console.WriteLine("Please enter a valid number between 1 - {0}", numberOfColumns);
+                            int.TryParse(Console.ReadLine(), out choice);
+                        }
+
+                        if (Gameplay.turnCount % 2 == 0)
+                        {
+                            multiplayerGame.InsertToken(player1, choice);
+                            winner = multiplayerGame.Winner(player1);
+                            theWinner = player1.Name;
+                        }
+
+                        else
+                        {
+                            multiplayerGame.InsertToken(player2, choice);
+                            winner = multiplayerGame.Winner(player2);
+                            theWinner = player2.Name;
+                        }
+
+                        //Console formatting
+                        Console.WriteLine();
+                        Console.WriteLine();
+
+                        int numberOfCellsEmpty = multiplayerGame.Cells.IndexOf('#');
+                        if (numberOfCellsEmpty == -1)
+                        {
+                            Console.WriteLine("{0} and (1} have TIED!!", player1.Name, player2.Name);
+                            break;
+                        }
+                    } while (!winner);
+
+
+                    if(winner)
+                    {
+                        multiplayerGame.DrawBoard();
+                        Console.WriteLine("\nCongratulations {0}!!\nYOU WIN!!", theWinner);
                     }
-
-                    if (Gameplay.turnCount % 2 == 0)
-                        multiplayerGame.InsertToken(player1, choice);
+         
+                    
+                    Console.WriteLine("Would you like to play agian? (Y / N)");
+                    
+                    if (Console.ReadLine().ToUpper().Substring(0,1) == "Y")
+                        again = true;
                     else
-                        multiplayerGame.InsertToken(player2, choice);
-
-                } while (!winner);
-            }
+                        again = false;
+                }
+            } while (again);
         }
     }
 }
